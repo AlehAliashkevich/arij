bdb0a537202a428fd8666008d187e0159c28d9e3
IvanRekun <ivrekun0@gmail.com>
2023-11-18 18:14:27 +0500

    Update task.ts(enum)

--- shared/arij-common/src/models/task.ts
+++ shared/arij-common/src/models/task.ts
@@ -4,8 +4,8 @@ export interface Task<T = string> extends SystemRecord<T>, TaskInfo {
 	name: string;
 	description: string;
     status: Report enum Status{
-		To_Do = 'To do',
-		In_Progress = 'In progress',
-		Closed = 'Closed',
+		To_Do = 'To_do',
+		In_Progress = 'In_progress',
+		Closed = 'Closed'
 	}
 }
