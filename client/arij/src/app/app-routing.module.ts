13a6b28133bc76c4e505af7246f4b843297ac083
Sargon1983 <145935586+Sargon1983@users.noreply.github.com>
2023-11-20 15:39:01 +0300

    delete import calc input, output

--- client/arij/src/app/app-routing.module.ts
+++ client/arij/src/app/app-routing.module.ts
@@ -2,8 +2,6 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { CalculatorComponent } from './calculator/calculator.component';
 import { CalculatorModule } from './calculator/calculator.module';
-import { CalculatorOutputComponent } from './calculator/calculator-output/calculator-output.component';
-import { CalculatorInputComponent } from './calculator/calculator-input/calculator-input.component';
 import { ErrorPageNotFoundComponent } from './calculator/error-page-not-found/error-page-not-found.component';
 
 export const routes: Routes = [
