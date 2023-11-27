7c895970d322770002117df37a3df8df2e4e3bec
IvanRekun <ivrekun0@gmail.com>
2023-11-27 21:20:46 +0500

    change import

--- server/arij-api/src/neuro-worker/neuro-worker.resolver.ts
+++ server/arij-api/src/neuro-worker/neuro-worker.resolver.ts
@@ -2,7 +2,7 @@ import { Query, Resolver } from '@nestjs/graphql';
 import { CurrentUser } from 'src/auth/user.decorator';
 import { UserContext } from '@arij/common';
 import { NeuroWorkerService } from './neuro-worker.service';
-import { NeuroWorker } from './entities/nw.entity';
+import { NeuroWorker } from './schema/neuro-worker.schema';
 
 @Resolver()
 export class NeuroWorkerResolver {
