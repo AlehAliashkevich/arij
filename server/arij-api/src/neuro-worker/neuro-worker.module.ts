d289368169c5a76290cc14837d4bf9c9e1d29ae8
IvanRekun <ivrekun0@gmail.com>
2023-11-27 21:22:26 +0500

    update import neuro-workerschema

--- server/arij-api/src/neuro-worker/neuro-worker.module.ts
+++ server/arij-api/src/neuro-worker/neuro-worker.module.ts
@@ -3,7 +3,7 @@ import { ConfigModule } from '@nestjs/config';
 import { NeuroWorkerResolver } from './neuro-worker.resolver';
 import { NeuroWorkerService } from './neuro-worker.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { NeuroWorker } from './entities/nw.entity';
+import { NeuroWorker } from './schema/neuro-worker.schema';
 
 @Module({
 	imports: [
