942844b3faae1f28770dd1e231e59f9df02bf448
IvanRekun <ivrekun0@gmail.com>
2023-11-28 00:54:46 +0500

    fix errors in import schemas

--- server/arij-api/src/neuro-worker/neuro-worker.resolver.ts
+++ server/arij-api/src/neuro-worker/neuro-worker.resolver.ts
@@ -2,7 +2,7 @@ import { Query, Resolver } from '@nestjs/graphql';
 import { CurrentUser } from 'src/auth/user.decorator';
 import { UserContext } from '@arij/common';
 import { NeuroWorkerService } from './neuro-worker.service';
-import { NeuroWorker } from './schema/neuro-worker.schema';
+import { NeuroWorker } from './schemas/neuro-worker.schema';
 
 @Resolver()
 export class NeuroWorkerResolver {
--- server/arij-api/src/neuro-worker/neuro-worker.service.ts
+++ server/arij-api/src/neuro-worker/neuro-worker.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@nestjs/common';
 import { UserContext } from '@arij/common';
-import { NeuroWorker } from './schema/neuro-worker.schema';
+import { NeuroWorker } from './schemas/neuro-worker.schema';
 import { Repository } from 'typeorm';
 import { InjectRepository } from '@nestjs/typeorm';
 
