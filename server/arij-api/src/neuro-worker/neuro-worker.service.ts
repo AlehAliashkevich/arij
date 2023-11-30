f3cad7f3b673b7520cdcc04027f6d80f32160868
IvanRekun <ivrekun0@gmail.com>
2023-12-01 02:19:58 +0500

    fixed connection to mongo

--- server/arij-api/src/neuro-worker/neuro-worker.service.ts
+++ server/arij-api/src/neuro-worker/neuro-worker.service.ts
@@ -1,10 +1,10 @@
 import { Injectable } from '@nestjs/common';
 import { UserContext } from '@arij/common';
 import { NeuroWorker } from './schemas/neuro-worker.schema';
-import { Repository } from 'typeorm';
-import { InjectRepository } from '@nestjs/typeorm';
+import { Model } from 'mongoose';
+import { InjectModel } from '@nestjs/mongoose';
 
 @Injectable()
 export class NeuroWorkerService {
-//	constructor(@InjectRepository(NeuroWorker) private nwRepo: Repository<NeuroWorker>,) {}
+	constructor(@InjectModel(NeuroWorker.name) private itemModel: Model<NeuroWorker>) {}
 }
